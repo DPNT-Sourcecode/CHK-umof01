e009ed3f6ef4f67c3f032c108c505e2b1c9bb236
sbx_user1051 <sbx_user1051@169.254.98.5>
2024-05-01 11:24:45 +0000

    Wed May 01 11:24:45 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,39 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    # Define prices dictionary (could be defined in a different module for modularity)
+    prices_dict = {
+        'A': 50,
+        'B': 30,
+        'C': 20,
+        'D': 15
+    }
+    special_offers_dict = {
+        'A': {'quantity': 3, 'total_price': 130},
+        'B': {'quantity': 2, 'total_price': 45},
+    }
+
+    # Count items in basket and error if there are new items not present in LUTs
+    basket = {}
+    for sku in skus:
+        if not sku in prices_dict:
+            return -1
+        basket[sku] = basket.get(sku, 0) + 1
+
+    # Check if any of the items in the basket has a special offer and compute
+    # the offers first, sum the remainders. 
+    # Otherwise just add the standard item * price
+    final_checkout = 0
+    for sku, count in basket.items():
+        if sku in special_offers_dict:
+            total_packs = count // special_offers_dict[sku]['quantity']
+            remainder = count % special_offers_dict[sku]['quantity']
+            payment = total_packs * special_offers_dict[sku]['total_price'] + \
+                remainder * prices_dict[sku]
+        else: 
+            payment = count * prices_dict[sku]
+        final_checkout += payment
+            
+    return final_checkout
+
+
